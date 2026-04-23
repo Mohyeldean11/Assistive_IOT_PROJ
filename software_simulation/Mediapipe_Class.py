5340f06d7ebbea34f1d18ae7d6cbff37f5668ad8
Mohyeldean11 <mohie.badawy@opmobility.com>
2026-04-23 15:40:59 +0200

    cont

--- software_simulation/Mediapipe_Class.py
+++ software_simulation/Mediapipe_Class.py
@@ -6,6 +6,6 @@ class PoseProcessor :
         self.poselandmarker = mp.tasks.vision.PoseLandmarker
         poselandmarker_options = mp.tasks.vision.PoseLandmarkerOptions
         poseVision_Running = mp.tasks.vision.RunningMode
-        options = poselandmarker_options(base_options=baseoptions(model_asset_path = "Case_study\Assistive_IOT_PROJ\pose_landmarker_lite.task"),
+        options = poselandmarker_options(base_options=baseoptions(model_asset_path = "Case_study\Assistive_IOT_PROJ\Helper_Models\pose_landmarker_lite.task"),
                                 running_mode = poseVision_Running.IMAGE)
         
